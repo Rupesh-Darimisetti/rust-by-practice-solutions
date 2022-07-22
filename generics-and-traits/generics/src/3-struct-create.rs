7099b36376be1f41cac494a7c00c8c10cfb9b2cb
Rupesh-Darimisetti <rupeshdarimisetti@gmail.com>
2022-07-22 20:49:07 +0530

    add generics solution

---
+++ generics-and-traits/generics/src/3-struct-create.rs
@@ -0,0 +1,14 @@
+// 3. 🌟
+
+// Implement struct Point to make it work.
+struct Point<T,U>{
+    x: T,
+    y: U,
+}
+
+fn main() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+
+    println!("Success!");
+}
---
+++ generics-and-traits/generics/src/4-struct-change.rs
@@ -0,0 +1,14 @@
+// 4. 🌟🌟
+
+// Modify this struct to make the code work
+struct Point<T> {
+    x: T,
+    y: String,
+}
+
+fn main() {
+    // DON'T modify this code.
+    let p = Point{x: 5, y : "hello".to_string()};
+
+    println!("Success!");
+}
