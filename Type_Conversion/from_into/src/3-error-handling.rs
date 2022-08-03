3c63a3144ad919bedec1a987f3f2b7a3e298584c
Rupesh-Darimisetti <rupeshdarimisetti@gmail.com>
2022-08-03 21:32:41 +0530

    add form into solution

---
+++ Type_Conversion/from_into/src/3-error-handling.rs
@@ -0,0 +1,41 @@
+/**
+ * 3. 🌟🌟🌟 When performing error handling 
+ * it is often useful to implement From trait
+ *  for our own error type. 
+ * Then we can use ? to automatically convert 
+ * the underlying error type to our own error type.
+ */
+use std::fs;
+use std::io;
+use std::num;
+
+enum CliError {
+    IoError(io::Error),
+    ParseError(num::ParseIntError),
+}
+
+impl From<io::Error> for CliError {
+    // IMPLEMENT from method
+    fn from(error: io::Error) -> Self {
+        CliError::IoError(error)
+    }
+}
+
+impl From<num::ParseIntError> for CliError {
+    // IMPLEMENT from method
+    fn from(error: num::ParseIntError) -> Self{
+        CliError::ParseError(error)
+    }
+}
+
+fn open_and_parse_file(file_name: &str) -> Result<i32, CliError> {
+    // ? automatically converts io::Error to CliError
+    let contents = fs::read_to_string(&file_name)?;
+    // num::ParseIntError -> CliError
+    let num: i32 = contents.trim().parse()?;
+    Ok(num)
+}
+
+fn main() {
+    println!("Success!")
+}
