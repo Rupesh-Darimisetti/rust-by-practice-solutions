2f3c742551ad8571d9aa65e0992d9d2a8238da00
Rupesh-Darimisetti <rupeshdarimisetti@gmail.com>
2022-08-08 19:16:33 +0530

    add lifetime basic solution

---
+++ Lifetime/basic/src/2-scope.rs
@@ -0,0 +1,14 @@
+/* annotate `r` and `x` as above, and explain why this code fails to compile, in the lifetime aspect. */
+
+fn main() {  
+    {
+        let r;                // ---------+-- 'a
+                              //          |
+        {                     //          |
+            let x = 5;        // -+-- 'b  |
+            r = &x;           //  |       |
+        println!("r: {}", r); // -+       |
+                              //          |
+         }                    //          |
+        }                     // ---------+
+}
