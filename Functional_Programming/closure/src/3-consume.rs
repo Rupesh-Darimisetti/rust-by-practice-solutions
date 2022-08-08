483744ff5dd935ffe106877b98a267f8ee1a68a9
Rupesh-Darimisetti <rupeshdarimisetti@gmail.com>
2022-08-08 21:35:25 +0530

    add closure solution

---
+++ Functional_Programming/closure/src/3-consume.rs
@@ -0,0 +1,31 @@
+/* Make it work in two ways, none of them is to remove `take(movable)` away from the code
+*/
+fn main() {
+    let movable = Box::new(3);
+
+    let consume = || {
+        println!("`movable`: {:?}", movable);
+        take(&movable);
+    };
+
+    consume();
+    consume();
+}
+
+fn take<T>(_v: T) {}
+// or
+/* Make it work in two ways, none of them is to remove `take(movable)` away from the code
+*/
+fn main() {
+    let movable = Box::new(3);
+
+    let consume = || {
+        println!("`movable`: {:?}", movable);
+        take(movable);
+    };
+
+    consume();
+   //  consume();
+}
+
+fn take<T>(_v: T) {}
